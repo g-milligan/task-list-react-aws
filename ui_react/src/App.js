743e3e2eb7653d638897612ec9172ed0f6a7b9b8
Gregg Milligan II <milligangregg@gmail.com>
2024-05-14 11:59:28 -0400

    App.js Task.js UpdateTaskForm.js

--- ui_react/src/App.js
+++ ui_react/src/App.js
@@ -1,6 +1,7 @@
 import { AddTaskForm } from "./components/AddTaskForm";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Task } from "./components/Task";
 
 const darkTheme = createTheme({
   palette: {
@@ -8,11 +9,14 @@ const darkTheme = createTheme({
   },
 });
 
+const task = {id: '1', name: 'do dishes', completed: false};
+
 export default function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <AddTaskForm />
+      <Task task={task} />
     </ThemeProvider>
   );
 }
